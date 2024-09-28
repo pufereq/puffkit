f1b2c86e4ee8cee60ff2a7ae14ccde2970c4bc53
Artur Borecki <me@pufereq.pl>
2024-09-28 20:10:06 +0200

    chore(__init__.py): import `PkApp`

--- puffkit/__init__.py
+++ puffkit/__init__.py
@@ -4,6 +4,7 @@ __version__ = "0.0.0"
 
 print(f"puffkit {__version__}")
 
+from puffkit.app import PkApp
 from puffkit.color import palettes
 from puffkit.color.color import ColorValue, PkColor
 from puffkit.object import PkObject
