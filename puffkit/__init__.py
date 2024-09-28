c48215d4753fb3b62c35240d818bd47224e609ae
Artur Borecki <me@pufereq.pl>
2024-09-28 20:07:24 +0200

    chore(__init__.py): import `PkScene`

--- puffkit/__init__.py
+++ puffkit/__init__.py
@@ -8,6 +8,7 @@ from puffkit.color import palettes
 from puffkit.color.color import ColorValue, PkColor
 from puffkit.object import PkObject
 from puffkit.rect import PkRect
+from puffkit.scene import PkScene
 from puffkit.subsurface import PkSubSurface
 from puffkit.surface import PkSurface
 from puffkit.textures import get_texture
