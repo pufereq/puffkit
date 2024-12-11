5de80afd4deadfe720dc2a08fc52977beb62131d
Artur Borecki <me@pufereq.pl>
2024-12-11 20:26:26 +0100

    feat(event_manager.py): add the event manager `PkEventManager`

---
+++ puffkit/event/event_manager.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""Event manager module for puffkit."""
+
+from __future__ import annotations
+
+import logging as lg
+import pygame as pg
+
+from typing import Any
+
+from puffkit import PkApp
+from puffkit.event import PkEvent
+
+
+class PkEventManager:
+    """Event manager class for puffkit.
+
+    The event manager is responsible for managing the events in the app.
+    """
+
+    def __init__(self, app: PkApp) -> None:
+        """Initialize the event manager class."""
+        self.logger = lg.getLogger(f"{__name__}.{self.__class__.__name__}")
+        self.logger.debug("Initializing event manager...")
+
+        self.app = app
+
+        self.events: list[PkEvent] = []
+        self.handlers: dict[int, Any] = {}
+
+    def add_handler(self, event_type: int, handler: Any) -> None:
+        """Add (or overwrite) a handler to the event manager."""
+        if event_type in self.handlers:
+            self.logger.warning(f"Overwriting handler for event type {event_type}...")
+        self.handlers[event_type] = handler
+
+    def remove_handler(self, event_type: int) -> None:
+        """Remove a handler from the event manager."""
+        if event_type in self.handlers:
+            del self.handlers[event_type]
+            self.logger.info(f"Removed handler for event type {event_type}.")
+        else:
+            self.logger.warning(f"No handler found for event type {event_type}.")
+
+    def handle_events(self, event_list: list[PkEvent]) -> None:
+        """Handle the events in the event manager."""
+        for event in event_list:
+            if event.type in self.handlers:
+                self.handlers[event.type](event)
+
+    def update(self, dt: float) -> None:
+        """Update the event manager."""
+        self.events = [PkEvent.from_pygame(e) for e in pg.event.get()]
+        self.handle_events(self.events)
